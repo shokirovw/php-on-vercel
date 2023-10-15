bd50a7f12f9b4b3c07e499c810744ebfba3c2c9c
shokirovw <shokirowv@gmail.com>
2023-10-15 13:39:31 +0500

    Trying SQLlite3

--- api/index.php
+++ api/index.php
@@ -1,19 +1,20 @@
-<?php 
-    $n = rand(1, 10);
+<?php
 
-    $contents = file_get_contents("https://jsonplaceholder.typicode.com/posts/".$n);
+$db = new SQLite3('/tmp/db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
-    $obj = json_decode($contents, true);
-?>
+$db->query('CREATE TABLE IF NOT EXISTS "visits" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "url" VARCHAR,
+    "time" DATETIME
+)');
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP on Vercel</title>
-</head>
-<body>
-    <p style="color: red; text-align:center;"><?php echo $obj["title"]; ?></p>
-</body>
-</html>
+$statement = $db->prepare('INSERT INTO "visits" ("url", "time") VALUES (:url, :time)');
+$statement->bindValue(':url', ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? 'https') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+$statement->bindValue(':time', date('Y-m-d H:i:s'));
+$statement->execute();
+
+$visits = $db->querySingle('SELECT COUNT(id) FROM "visits"');
+
+echo("User visits: $visits");
+
+$db->close();
